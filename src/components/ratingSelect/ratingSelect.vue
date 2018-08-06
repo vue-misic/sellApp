6f970ca0559b719299e6b334a1d3e9db6d83a3be
zhaokailei <zhaokailei@aspirecn.com>
2018-08-06 23:08:43 +0800

    选择组件
    zkl

--- src/components/ratingSelect/ratingSelect.vue
+++ src/components/ratingSelect/ratingSelect.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="rating-select">
         <div class="rating-type">
-            <span>{{ desc.all }}</span>
-            <span>{{ desc.positive }}</span>
-            <span>{{ desc.negative }}</span>
+            <span class="block positive" :class="{active:selectType===2}">{{ desc.all }}<span class="count" :class="{active:selectType===2}">50</span></span>
+            <span class="block positive"  :class="{active:selectType===0}">{{ desc.positive }}<span class="count" :class="{active:selectType===0}">40</span></span>
+            <span class="block negative"  :class="{active:selectType===1}">{{ desc.negative }}<span class="count" :class="{active:selectType===1}">10</span></span>
         </div>            
-        <div class="rating-switch">
+        <div class="rating-switch" :class="{on: onlyContent}">
             <span class="icon-check_circle"></span>
             <span class="text">只看有内容的评价</span>
         </div>
@@ -55,6 +55,47 @@ export default {
             padding: 18px 0
             margin: 0 18px
             border-1px(rgba(7,17,27,0.1))
+            font-size: 0
+            .block
+                display: inline-block
+                padding: 8px 12px
+                margin-right: 8px
+                border-radius: 2px
+                background-color: rgba(0, 160, 220, 0.2)
+                font-size: 12px
+                line-height: 16px
+                color: rgb(77, 85, 93)
+                &.active
+                    background-color: rgb(0, 160, 220)
+                    color: #fff
+                &.negative
+                    background-color: rgba(77, 85, 93, 0.2)
+                    &.active
+                        background-color: rgb(77, 85, 93)
+                .count
+                    font-size: 8px
+                    margin-left: 2px
+        .rating-switch
+            width: 100%
+            box-sizing: border-box
+            padding: 12px 18px
+            border-bottom: 1px solid rgb(7, 17, 27, 0.1)
+            color: rgb(147, 153, 159)
+            &.on
+                .icon-check_circle
+                    color: #008c15
+            .icon-check_circle
+                display: inline-block
+                vertical-align: top
+                font-size: 24px
+                line-height 24px
+            .text
+                display: inline-block
+                font-size: 12px
+                line-height 24px
+
+
+
 </style>
 
 
